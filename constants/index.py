ba4fb0510a41a7a85f42e4ac1dafab69619d7f5e
TienDat57 <dangtiendat1135@gmail.com>
2023-01-11 23:01:26 +0700

    HOTFIX | fix question page

---
+++ constants/index.py
@@ -0,0 +1,6 @@
+list_questions = [
+   'Thống kê số phòng trọ theo quận. Và xem thử các quận nào có tỷ lệ trọ nhiều, tại sao lại có điều này( trường học, khu công nghiệp) :sunglasses:',
+   'Thời gian đăng cho thuê phòng trọ chủ yếu vào các tháng nào? Vì sao lại như vậy (dựa vào tháng tựu trường, tháng hè, khu công nghiệp..) :sunglasses:',
+   'Giá tiền trọ theo năm. Tại sao mỗi năm lại có sự tăng giảm như vậy( visualize). :sunglasses:',
+   'Giá tiền trọ theo quận và thống kê số diện tích của các phòng trọ (đa số các phòng trọ chiếm diện tích bao nhiêu) và liên kết giữa số tiền và diện tích của mỗi quận (quận đó thuộc dạng đắc đỏ hay trung bình). Vì sao lại như vậy.',
+]
--- pages/question.py
+++ pages/question.py
@@ -1,4 +1,27 @@
 import streamlit as st
 
+from components import question_item
+
 def app():
    st.title("Asking meaningful questions that need to be answered")
+   option = st.selectbox(
+      'How would you like to be contacted? 💔',
+      ('Question 1', 'Question 2', 'Question 3', 'Question 4', 'Question 5', 'Question 6', 'Question 7', 'Question 8')
+   )
+   
+   if option == 'Question 1':
+      question_item.question_one()
+   elif option == 'Question 2':
+      question_item.question_two()
+   elif option == 'Question 3':
+      question_item.question_three()
+   elif option == 'Question 4':
+      question_item.question_four()
+   # elif option == 'Question 5':
+   #    question(5)
+   # elif option == 'Question 6':
+   #    question(6)
+   # elif option == 'Question 7':
+   #    question(7)
+   # elif option == 'Question 8':
+   #    question(8)
